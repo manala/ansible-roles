7ec09c7542c7996f73a0eb4dc919b1c8ec79f1ff
Lod <46480794+greedybro@users.noreply.github.com>
2019-11-29 10:23:43 +0100

    [All]Collection (#389)
    * Init
    
    * [All] Lookup to query
    
    * collection

---
+++ prepare_collection.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import glob
+import re
+
+
+for paths in ['./plugins/lookup', './plugins/callback', './plugins/filter']:
+	if not os.path.exists(paths):
+		os.makedirs(paths)
+
+for files in glob.glob('./roles/*/lookup_plugins/*.py'):
+	shutil.move(files, './plugins/lookup')
+
+for files in glob.glob('./roles/*/callback_plugins/*.py'):
+	shutil.move(files, './plugins/callback')
+
+for files in glob.glob('./roles/*/filter_plugins/*.py'):
+	shutil.move(files, './plugins/filter')
+
+for filename in glob.glob( "./plugins/*/*.py" ):
+	new_name = re.sub(r'manala_', r'', filename)
+	os.rename(filename, new_name)
+
+for paths in ['./roles/*/*_plugins/',
+			'./roles/*/.manala/',
+			'./roles/*/.travis.yml',
+			'./roles/*/Makefile',
+			'./roles/*/.gitignore',
+			'./roles/*/tests/',
+			'./.env.dist',
+			'./.gitsplit.yml',
+			'./.travis.yml',
+			'./README.md']:
+	for path in glob.glob(paths):
+		if os.path.isfile(path):
+			os.remove(path)
+		else:
+			shutil.rmtree(path)
+
+files = [f for f in glob.glob('./roles/*/tasks/*.yml', recursive=True)]
+regex = re.compile(r'(lookup\(\s*\')manala_', re.DOTALL)
+
+for file_path in files:
+	with open(file_path, 'r+') as file:
+		file_data = file.read()
+		if len(re.findall(regex, file_data)):
+			file.seek(0)
+			file.write(re.sub(regex, r"\1manala.roles.", file_data))
+			file.truncate
