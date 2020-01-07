13d18f970799b8805e8372c3c328c39f579dc006
Lod <lod.lapeyre@elao.com>
2020-01-07 15:35:06 +0100

    Update requirement.yml + clean script

--- prepare_collection.py
+++ prepare_collection.py
@@ -11,21 +11,21 @@ for paths in ['./plugins/lookup', './plugins/callback', './plugins/filter', './p
 		os.makedirs(paths)
 
 for files in glob.glob('./roles/*/lookup_plugins/*.py'):
-	shutil.move(files, './plugins/lookup')
+    shutil.move(files, './plugins/lookup')
 
 for filename in glob.glob('./plugins/lookup/*.py'):
 	new_name = re.sub(r'manala_', r'', filename)
 	os.rename(filename, new_name)
 
 for files in glob.glob('./roles/*/callback_plugins/*.py'):
-	shutil.move(files, './plugins/callback')
+    shutil.move(files, './plugins/callback')
 
 for filename in glob.glob('./plugins/callback/*.py'):
 	new_name = re.sub(r'manala_', r'', filename)
 	os.rename(filename, new_name)
 
 for files in glob.glob('./roles/*/filter_plugins/*.py'):
-	shutil.move(files, './plugins/filter')
+    shutil.move(files, './plugins/filter')
 
 for filename in glob.glob('./plugins/filter/*.py'):
 	new_name = re.sub(r'manala_', r'', filename)
@@ -55,9 +55,9 @@ files = [f for f in glob.glob('./roles/*/tasks/*.yml', recursive=True)]
 regex = re.compile(r'(query\(\s*\')manala_', re.DOTALL)
 
 for file_path in files:
-	with open(file_path, 'r+') as file:
-		file_data = file.read()
-		if len(re.findall(regex, file_data)):
-			file.seek(0)
-			file.write(re.sub(regex, r"\1manala.roles.", file_data))
-			file.truncate
+    with open(file_path, 'r+') as file:
+        file_data = file.read()
+        if len(re.findall(regex, file_data)):
+            file.seek(0)
+            file.write(re.sub(regex, r"\1manala.roles.", file_data))
+            file.truncate
--- roles/bind/filter_plugins/manala_bind.py
+++ roles/bind/filter_plugins/manala_bind.py
@@ -3,6 +3,6 @@ class FilterModule(object):
         return {
             'manala_bind_zone_file': self.manala_bind_zone_file
         }
- 
+
     def manala_bind_zone_file(self, value):
         return 'db.' + value.replace('.in-addr.arpa', '')
