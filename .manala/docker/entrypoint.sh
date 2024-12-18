0ca5c2ba504a1963148e259cf69e607162f712b7
Lod <lod@rix.fr>
2024-12-18 10:55:33 +0100

    Manala up - introduce zsh
    Add a message after `make sh` to remind that containerd is not compatible with molecule (for test on Mac Silicon)

--- .manala/docker/entrypoint.sh
+++ .manala/docker/entrypoint.sh
@@ -5,11 +5,12 @@ set -e
 # Ssh agent bridge
 if [ -n "${SSH_AUTH_SOCK}" ]; then
   sh -c " \
-    while sleep 1; do \
+    while true; do \
       rm -f /var/run/ssh-auth-bridge.sock ;
       socat \
         UNIX-LISTEN:/var/run/ssh-auth-bridge.sock,fork,mode=777 \
         UNIX-CONNECT:/var/run/ssh-auth.sock ; \
+      sleep 1; \
     done \
   " &
 fi
@@ -17,11 +18,12 @@ fi
 # Docker bridge
 if [ -n "${DOCKER_HOST}" ]; then
   sh -c " \
-    while sleep 1; do \
+    while true; do \
       rm -f /var/run/docker-bridge.sock ;
       socat -t 600 \
         UNIX-LISTEN:/var/run/docker-bridge.sock,fork,mode=777 \
         UNIX-CONNECT:/var/run/docker.sock ; \
+      sleep 1; \
     done \
   " &
 fi
